572566571d2c36ca8e1f997a40ae36b54d612ca5
MacarenaCarrero <macarena.c.rios@gmail.com>
2025-04-22 18:03:52 +0200

    rotatesuputamadre

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,62 @@
-const date = new Date();
-const hourElement = document.getElementById('hour');
-const dayWeekElement = document.getElementById('dayWeek');
-const minElement = document.getElementById('min');
+const hoursElement = document.getElementById('hours');
+const dayNameWeekElement = document.getElementById('dayWeek');
+const dayNumberWeekElement = document.getElementById('dayNumberWeek');
+const nameMonthElement = document.getElementById('nameMonth');
+const minsElement = document.getElementById('mins');
 const secondsElement = document.getElementById('seconds');
 const yearElement = document.getElementById('year');
+const drawHoursElement = document.getElementById('drawHours');
+const drawDatesElement = document.getElementById('drawDates');
 
 const drawHour = () => {
-  hourElement.textContent = date.getHours();
-  minElement.textContent = date.getMinutes();
-};
-drawHour();
+  const date = new Date();
+  let hoursElement = date.getHours();
+  let minutesElement = date.getMinutes();
+  let secondsElement = date.getSeconds();
 
-const drawDate = () => {
-  dayWeekElement.textContent = date.getDate();
-  yearElement.textContent = date.getFullYear();
+  hoursElement = hoursElement < 10 ? '0' + hoursElement : hoursElement;
+  minutesElement = minutesElement < 10 ? '0' + minutesElement : minutesElement;
+  secondsElement = secondsElement < 10 ? '0' + secondsElement : secondsElement;
+
+  drawHoursElement.textContent = `${hoursElement}:${minutesElement}`;
 };
-drawDate();
-const getMin = setInterval(() => {}, 1000);
+
+const getDate = () => {
+  const date = new Date();
+  const days = [
+    'Domingo',
+    'Lunes',
+    'Martes',
+    'Miércoles',
+    'Jueves',
+    'Viernes',
+    'Sábado'
+  ];
+  const months = [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre'
+  ];
+  let dayNameWeekElement = days[date.getDay()];
+  let dayNumberWeekElement = date.getDate();
+  let nameMonthElement = months[date.getMonth()];
+  let yearElement = date.getFullYear();
+
+  drawDatesElement.textContent = `${dayNameWeekElement} ${dayNumberWeekElement} ${nameMonthElement} ${yearElement}`;
+};
+
+setInterval(() => {
+  drawHour();
+  getDate();
+}, 1000);
+drawHour();
+getDate();
